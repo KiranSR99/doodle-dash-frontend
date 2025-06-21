59613edf5328dd46d84f3ae4e85333fda6b8e754
Mahendra Shrestha <mahendra@Mahendras-MacBook-Air.local>
2025-06-21 18:57:15 +0545

    api url changed to use environment's url

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class SocketService {
   private socket: Socket;
+  private readonly apiUrl = environment.apiUrl;
 
   constructor() {
-    this.socket = io('http://127.0.0.1:5000', {
+    this.socket = io(this.apiUrl, {
       transports: ['websocket'],
     });
   }
